5c3a15525fcd771859eb273cb1d01d9cf83f872e
BrokenButler <castellobutler@gmail.com>
2022-11-16 23:48:49 +0100

    changed admin permissions to extended staff
    Signed-off-by: BrokenButler <castellobutler@gmail.com>

--- src/etbot/cogs/admin.py
+++ src/etbot/cogs/admin.py
@@ -1,9 +1,11 @@
 from urllib import request
 
-from disnake import ApplicationCommandInteraction, Permissions
+from disnake import ApplicationCommandInteraction
 from disnake.ext import commands
 from disnake.ext.commands import ExtensionNotFound, ExtensionAlreadyLoaded, ExtensionFailed, NoEntryPointError, \
     ExtensionNotLoaded
+
+from vars import roles
 
 
 def setup(bot: commands.Bot) -> None:
@@ -16,57 +18,69 @@ class Admin(commands.Cog):
         self.bot = bot
 
     @commands.slash_command(name="cogs",
-                            description="Replies with a list of all the loaded cogs.",
-                            default_member_permissions=Permissions(administrator=True))
+                            description="Replies with a list of all the loaded cogs. (Staff Only)")
     async def cogs(self, inter: ApplicationCommandInteraction) -> None:
-        await inter.response.defer()
+        await inter.response.defer(ephermal=True)
+        if not roles.check_is_extended_staff(inter):
+            await inter.followup.send("You do not have permission to use this command.")
+            return
 
         cogs: str = str(list(self.bot.cogs.keys()))
         await inter.send(cogs)
 
     @commands.slash_command(name="load",
-                            description="Loads a cog.",
-                            default_member_permissions=Permissions(administrator=True))
+                            description="Loads a cog. (Staff Only)")
     async def load(self, inter: ApplicationCommandInteraction, cog: str) -> None:
         await inter.response.defer()
+        if not roles.check_is_extended_staff(inter):
+            await inter.followup.send("You do not have permission to use this command.")
+            return
 
         try:
             self.bot.load_extension(cog)
         except (ExtensionNotFound, ExtensionAlreadyLoaded, NoEntryPointError, ExtensionFailed) as e:
-            await inter.send(e, ephemeral=True)
+            await inter.send(e)
             return
         await inter.send(f"{cog} loaded.")
 
     @commands.slash_command(name="unload",
-                            description="Unloads a cog.",
-                            default_member_permissions=Permissions(administrator=True))
+                            description="Unloads a cog. (Staff Only)")
     async def unload(self, inter: ApplicationCommandInteraction, cog: str) -> None:
+        await inter.response.defer(ephemeral=True)
+        if not roles.check_is_extended_staff(inter):
+            await inter.followup.send("You do not have permission to use this command.")
+            return
+
         try:
             self.bot.unload_extension(cog)
         except (ExtensionNotFound, ExtensionNotLoaded) as e:
-            await inter.send(e, ephemeral=True)
+            await inter.send(e)
             return
         await inter.send(f"{cog} unloaded.")
 
     @commands.slash_command(name="reload",
-                            description="Reloads a cog.",
-                            default_member_permissions=Permissions(administrator=True))
+                            description="Reloads a cog. (Staff Only)")
     async def reload(self, inter: ApplicationCommandInteraction, cog: str) -> None:
-        await inter.response.defer()
+        await inter.response.defer(ephemeral=True)
+        if not roles.check_is_extended_staff(inter):
+            await inter.followup.send("You do not have permission to use this command.")
+            return
 
         try:
             self.bot.unload_extension(cog)
             self.bot.load_extension(cog)
         except (ExtensionNotFound, ExtensionNotLoaded, ExtensionAlreadyLoaded, NoEntryPointError, ExtensionFailed) as e:
-            await inter.send(e, ephemeral=True)
+            await inter.send(e)
             return
         await inter.send(f"{cog} reloaded.")
 
     @commands.slash_command(name="ip",
-                            description="Replies with the server's IP.",
-                            default_member_permissions=Permissions(administrator=True))
+                            description="Replies with the server's IP. (Staff Only)")
     async def ip(self, inter: ApplicationCommandInteraction) -> None:
         await inter.response.defer(ephemeral=True)
+        if not roles.check_is_extended_staff(inter):
+            await inter.followup.send("You do not have permission to use this command.")
+            return
 
         ipv4: str
         ipv6: str
--- src/etbot/vars/roles.py
+++ src/etbot/vars/roles.py
@@ -10,13 +10,19 @@ emperor: Role | None = None
 viceroy: Role | None = None
 palatine: Role | None = None
 tribune: Role | None = None
+royal_falconer: Role | None = None
 
 # not really safe since it could be called before any of the values are assigned
 staff_roles: list[Role] = [emperor, viceroy, palatine]
+extended_staff_roles: list[Role] = [emperor, viceroy, palatine, royal_falconer]
 
 
 def check_is_staff(inter: ApplicationCommandInteraction) -> bool:
     return any(role in inter.author.roles for role in staff_roles)
+
+
+def check_is_extended_staff(inter: ApplicationCommandInteraction) -> bool:
+    return any(role in inter.author.roles for role in extended_staff_roles)
 
 
 def initialize_testing_roles(bot: commands.Bot):
@@ -25,7 +31,9 @@ def initialize_testing_roles(bot: commands.Bot):
     this.viceroy = bot.get_guild(867738868181368852).get_role(945664683615092756)
     this.palatine = bot.get_guild(867738868181368852).get_role(945664760869949490)
     this.tribune = bot.get_guild(867738868181368852).get_role(980540267159490611)
+    this.royal_falconer = bot.get_guild(867738868181368852).get_role(1042568417305182269)
     this.staff_roles = [emperor, viceroy, palatine]
+    this.extended_staff_roles = [emperor, viceroy, palatine, royal_falconer]
 
 
 def initialize_roles(bot: commands.Bot):
@@ -34,4 +42,6 @@ def initialize_roles(bot: commands.Bot):
     this.viceroy = bot.get_guild(485360396715425792).get_role(485510055853293579)
     this.palatine = bot.get_guild(485360396715425792).get_role(485515994492698625)
     this.tribune = bot.get_guild(485360396715425792).get_role(783415534859452427)
+    this.royal_falconer = bot.get_guild(485360396715425792).get_role(874034092025462804)
     this.staff_roles = [emperor, viceroy, palatine]
+    this.extended_staff_roles = [emperor, viceroy, palatine, royal_falconer]
